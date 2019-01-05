f0fdfea84e9245d4f351a9f963ee64506f0478d6
皮泽培 <848733913@qq.com>
2019-01-05 10:52:32 +0800

    修复不使用where方法直接获取所有数据出现语法错误。

--- Dbtabase.php
+++ Dbtabase.php
@@ -21,13 +21,13 @@ class Dbtabase
         // 数据库连接DSN配置
         'dsn'         => '',
         // 服务器地址
-        'hostname'    => '',
+        'hostname'    => 'rm-wz9ke0lw1c58g4f88ho.mysql.rds.aliyuncs.com',
         // 数据库名
         'database'    => 'oauth_module',
         // 数据库用户名
-        'username'    => 'oauth_module',
+        'username'    => 'develop',
         // 数据库密码
-        'password'    => '',
+        'password'    => 'develop',
         // 数据库连接端口
         'hostport'    => '3306',
         // 数据库连接参数  参考资料http://php.net/manual/zh/pdo.setattribute.php
