51b2dca9bc14cd6cdf16aa32bdd4017e234cff06
皮泽培 <848733913@qq.com>
2019-02-01 18:02:37 +0800

    确定SaaS模式的规划方向

--- InitializeConfig.php
+++ InitializeConfig.php
@@ -12,5 +12,15 @@ namespace pizepei\config;
 
 class InitializeConfig
 {
-
+    /**
+     * 实例化
+     *      判断模式
+     *          web 传统模式
+     *          saas 模式
+     *      创建模式目录
+     *      创建应用目录
+     *      获取配置设置
+     *      合并配置
+     *      写入对应配置（注意命名空间）
+     */
 }
