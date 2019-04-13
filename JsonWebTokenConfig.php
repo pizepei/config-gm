01709b748b60d429f6fa9eea24237a5eb38b7e8e
皮泽培 <848733913@qq.com>
2019-04-13 18:12:05 +0800

    jwt支持

--- JsonWebTokenConfig.php
+++ JsonWebTokenConfig.php
@@ -17,11 +17,27 @@ class JsonWebTokenConfig
      */
     CONST JSON_WEB_TOKEN_SECRET =[
         /**
-         * name 名字 value 密钥   role 角色标识  alg 加密方法  Payload 内容
-         * 密钥代表的等级
+         * name 名字 secret 密钥   role 角色标识  alg 加密方法  Payload 内容
          */
         'common'=>[
-            'name'=>'普通权限','value'=>'abcd123343', 'alg'=>'md5','role'=>'common','Payload'=>'common'
+            'name'=>'普通权限',
+            'secret'=>'abcd123343',
+            'secret_key'=>'sakjshkjkljm889289023dscponbxba3242903902ijkds',//Header alg 为 aes 时需要
+            'token_name'=>'token',
+            'alg'=>'md5',
+            'role'=>'common',
+            'Header'=>[
+                'alg'=>'aes',//加密方法（默认base64_encode   可选ase）
+                'sig'=>'md5',//签名方法（默认MD5 一般根据secret 的alg设置）可选sha1
+                'typ'=>'JWT',//签名类型
+                'appid'=>'ashaaasd1232jjdskhkkx',//必须
+            ],
+            'Payload'=>[//jwtPayload
+                        'iss'=>'server',//签发人
+                        'exp'=>7200,//过期时间s
+                        'sub'=>'phpServer--socketServer',//主题
+                        'aud'=>'socketServer',//受众
+            ],
         ],
         'base'=>['name'=>'管理员','value'=>'', 'alg'=>'md5','role'=>''],
         'visitor'=>['name'=>'游客','value'=>'', 'alg'=>'md5','role'=>''],
@@ -31,28 +47,8 @@ class JsonWebTokenConfig
      */
     CONST JSON_WEB_TOKEN_ROLE = [
         'common'=>[],
+        'base'=>[],
+        'visitor'=>[],
     ];
-    /**
-     * Header
-     */
-    CONST JSON_WEB_TOKEN_HEADER = [
-        'alg'=>'md5',//加密方法（默认MD5 一般根据secret 的alg设置）
-        'typ'=>'JWT',//签名类型
-    ];
-    /**
-     *jwtPayload
-     */
-    CONST JSON_WEB_TOKEN_PAYLOAD = [
-        'common'=>[
-            'iss'=>'server',//签发人
-            'exp'=>7200,//过期时间s
-            'sub'=>'phpServer--socketServer',//主题
-            'aud'=>'socketServer',//受众
-            'nbf'=>0,//生效时间
-            'iat'=>0,//签发时间
-            'jti'=>'',//编号(随机数)
-        ],
-    ];
-    CONST JSON_WEB_TOKEN_NAME = 'token';
 
 }
